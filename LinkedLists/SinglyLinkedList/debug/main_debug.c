6788e4d49efa19c6029fcd96f47897a77ef6baa6
lagonza8 <official.lag.90@gmail.com>
2026-05-25 08:15:54 -0700

    Add file with tests and source code for singly linked lists

---
+++ LinkedLists/SinglyLinkedList/debug/main_debug.c
@@ -0,0 +1,166 @@
+/*
+ * Testing the singly linked list abstract data type.
+ * Unity build
+ *
+ * Flags:
+ * -std=c99             : Use the best C standard
+ * -fsanitize=address   : address sanitizer enabled
+ * -Wall                : All warnings are displayed
+ * -Werror              : All warnings lead to compile-time errors
+ * -g3                  : Add the most debugger info to file
+ * -O0                  : No compiler optimizations
+ *
+ * Compilation command:
+ * gcc debug/main_debug.c -o debug/bin/main -std=c99 -fsanitize=address -Wall -Werror -g3
+ *
+ * Run command: ./debug/bin/main
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include "../src/singly_linked_list.c"
+
+// Macro for safely freeing pointers, plus a forward declaration used by the macro
+#define safe_free(ptr) safer_free((void **)&ptr)
+void safer_free(void **ptr);
+
+int main(void) {
+
+  puts("\nSingly Linked List Abstract Data Type");
+
+  //Allocate memory for the list structure
+  SLList *my_list;
+  if ( (my_list = (SLList *)(malloc(sizeof(SLList)))) == NULL ) {
+    puts("Memory could not be allocated for list structure.");
+    return -1;
+  }
+
+  // Testing the list initialization function.
+  sl_list_init(my_list, free);
+  assert(sl_list_size(my_list) == 0); 
+  assert(sl_list_tail(my_list) == NULL);
+  assert(sl_list_head(my_list) == NULL);
+
+
+  // Declaring a pointer for integer data to be inserted into nodes/elements of the list
+  int8_t *data;
+
+  //allocating memory for each floating point value
+  if ( (data = (int8_t *)malloc(sizeof(int8_t))) == NULL ) {
+    puts("Failed to allocate memory for new datum.");
+    return -1;
+  }
+  *data = 8;
+
+  // Trying to remove element from an empty list returns value of -1
+  assert(sl_list_rem_next(my_list, NULL, (void **)&data) == -1);
+
+  // Inserting the first element
+  assert(sl_list_ins_next(my_list, NULL, data) == 0);
+  assert(sl_list_size(my_list) == 1); 
+
+  // Confirming that the first element is both the tail and head of the list.
+  assert(sl_list_is_tail(sl_list_head(my_list)) == 1);
+  assert(sl_list_is_head(my_list, sl_list_head(my_list)) == 1);
+
+  // Confirming the node contains the correct data
+  assert( *(int8_t *)sl_list_data(sl_list_head(my_list)) == 8);
+
+  /* Picture of linked list:
+  
+       Head -> 6.0 -> NULL
+  */
+
+  // Allocating memory for a new value
+  if ( (data = (int8_t *)malloc(sizeof(int8_t))) == NULL ) {
+    puts("Failed to allocate memory for new datum.");
+    return -1;
+  }
+  // Assigning value to the pointer and creating new head element of the list
+  *data = -50;
+  assert(sl_list_ins_next(my_list, NULL, data) == 0);
+  assert(sl_list_size(my_list) == 2);
+  assert( *(int8_t *)sl_list_data(sl_list_head(my_list)) == -50);
+  assert(sl_list_next(sl_list_tail(my_list)) == NULL);
+  assert(sl_list_next(sl_list_head(my_list)) == sl_list_tail(my_list));
+  assert(sl_list_next(sl_list_head(my_list)) != NULL);
+   
+
+  // Picture of linked list:
+  // Head -> -5.0 -> 6.0-> NULL
+
+  // Allocating memory for a new value
+  if ( (data = (int8_t *)malloc(sizeof(int8_t))) == NULL ) {
+    puts("Failed to allocate memory for new datum.");
+    return -1;
+  } 
+  // Assigning value to the pointer and creating new tail element of the list
+  *data = 100;
+  assert(sl_list_ins_next(my_list, sl_list_tail(my_list), data) == 0);
+  assert(sl_list_size(my_list) == 3);
+  assert( *(int8_t *)sl_list_data(sl_list_tail(my_list)) == 100);
+  assert(sl_list_next(sl_list_tail(my_list)) == NULL);
+  assert(sl_list_next(sl_list_head(my_list)) != NULL);
+  
+
+  // Picture of linked list:
+  // Head -> 5.0 -> 6.0 -> 3.0 -> NULL
+
+  // Allocating memory for a new value
+  if ( (data = (int8_t *)malloc(sizeof(int8_t))) == NULL ) {
+    puts("Failed to allocate memory for new datum.");
+    return -1;
+  }
+  // Assigning value to the pointer and creating new element after the head of the list
+  *data = -48;
+  assert(sl_list_ins_next(my_list, sl_list_head(my_list), data) == 0);
+  assert(sl_list_size(my_list) == 4);
+  assert( *(int8_t *)sl_list_data(sl_list_next(sl_list_head(my_list))) == -48);
+  assert(sl_list_next(sl_list_tail(my_list)) == NULL);
+  assert(sl_list_next(sl_list_head(my_list)) != NULL);
+
+
+  // Picture of linked list:
+  // Head -> 5.0 -> 4.0 -> 6.0 -> 3.0 -> NULL
+
+  /* Removing elements manually */
+  int8_t *value_removed;
+  
+  // Removing the head element
+  assert( sl_list_rem_next(my_list, NULL, (void **)&value_removed) == 0);
+  printf("Value removed from list: %d\n\n", *value_removed);
+  safe_free(value_removed);
+
+
+  // Picture of linked list:
+  // Head -> 4.0 -> 6.0 -> 3.0 -> NULL
+
+  // Removing after tail, should produce error message.
+  assert(sl_list_rem_next(my_list, my_list->tail, (void **)&value_removed) == -1);
+
+
+  // Removing the element after the head element
+  assert(sl_list_rem_next(my_list, my_list->head, (void **)&value_removed) == 0);
+  printf("Value removed from list: %d\n\n", *value_removed);
+  safe_free(value_removed);
+
+  // Picture of linked list:
+  // Head -> 4.0 -> 3.0 -> NULL
+
+  // Destroying the list after it is not needed.
+  sl_list_destroy(my_list); //frees element structs, and data pointer
+  safe_free(my_list); //frees list pointer
+  safe_free(value_removed); //frees pointer that returned data stored by deleted nodes
+
+return 0;
+}
+
+/* */
+void safer_free(void **ptr) {
+  if( ptr != NULL && *ptr != NULL ) {
+    free(*ptr);
+    *ptr = NULL;
+  }
+}
---
+++ LinkedLists/SinglyLinkedList/src/singly_linked_list.c
@@ -0,0 +1,145 @@
+/* Singly linked list abstract data type */
+#include <stdio.h>
+#include <string.h>
+
+// Node for a singly linked list
+typedef struct SinglyLinkedListNode_ {
+  void *data;
+  struct SinglyLinkedListNode_ *next;
+} SLLNode;
+
+// A structure representing a singly linked list
+typedef struct SinglyLinkedList_ {
+  size_t size;
+  SLLNode *tail;
+  SLLNode *head;
+  void (*destroy)(void *data);
+} SLList;
+
+/* Public interface for the ADT */
+// Function forward declarations
+void sl_list_init(SLList *list, void (*destroy)(void *data));
+void sl_list_destroy(SLList *list);
+int sl_list_ins_next(SLList *list, SLLNode *node, const void *data);
+int sl_list_rem_next(SLList *list, SLLNode *node, void **data);
+
+// Macros
+#define sl_list_size(list) ((list)->size)
+#define sl_list_tail(list) ((list)->tail)
+#define sl_list_head(list) ((list)->head)
+#define sl_list_is_tail(node) ((node)->next == NULL ? 1: 0)
+#define sl_list_is_head(list, node) ((node) == (list)->head ? 1: 0)
+#define sl_list_data(node) ((node)->data)
+#define sl_list_next(node) ((node)->next)
+
+
+// Function implementations
+void sl_list_init(SLList *list, void (*destroy)(void *data)) {
+  printf("Initializing list... ");
+  list->size = 0;
+  list->tail = NULL;
+  list->head = NULL;
+  list->destroy = destroy;
+  puts("Initialization completed.");
+return;
+}
+
+void sl_list_destroy(SLList *list) {
+  void *deleted_data;
+
+  printf("List destruction in progress... ");
+  while( sl_list_size(list) > 0 ) {
+    if( (sl_list_rem_next(list, NULL, (void **)&deleted_data) == 0) && (list->destroy != NULL) ) {
+      printf("A node has been removed... ");
+      list->destroy(deleted_data);
+      puts("The node's data has been released."); 
+    }
+  }
+
+  memset(list, 0, sizeof(SLList));
+  puts("List destruction completed.");
+
+return;
+}
+
+int sl_list_ins_next(SLList *list, SLLNode *node, const void *data) {
+  //Allocate memory for the new node
+  SLLNode *new_node;
+  if( (new_node = (SLLNode *)malloc(sizeof(SLLNode))) == NULL ) {
+    puts("Could not allocate memory for a new node.");
+    return -1;
+  }
+  new_node->data = (void *)data;
+
+  //Inserting new node at the head of list
+  if( node == NULL ) {
+    printf("Inserting new node at the head of the list... ");
+    if( sl_list_size(list) == 0 ) {
+      list->tail = new_node;
+    }
+    new_node->next = list->head;
+    list->head = new_node;
+  }
+  //Inserting new node elsewhere
+  else {
+    printf("Inserting a new node in the list... ");
+    if( node->next == NULL ) {
+      list->tail = new_node;
+      printf("The list will have a new tail... ");
+    }
+    new_node->next = node->next;
+    node->next = new_node;
+  }
+
+  puts("Insertion completed.");
+  list->size++;
+
+return 0;
+}
+
+int sl_list_rem_next(SLList *list, SLLNode *node, void **data) {
+  //Cannot remove a node from an empty list
+  if( sl_list_size(list) == 0 ) {
+    puts("Cannot remove a node from an empty list.");
+    return -1;
+  }
+  
+  SLLNode *deleted_node;
+
+  //Sentinal value is NULL for second argument, indicates removal of head node
+  if( node == NULL ) {
+    printf("Deletion of head node in progress... ");
+    *data = list->head->data;
+    deleted_node = list->head;
+    list->head = list->head->next;
+    
+    if( sl_list_size(list) == 1 ) {
+      list->tail = NULL;
+    }
+  }
+  //Otherwise, the node is being removed from elsewhere in the list
+  else {
+    //Cannot remove a node that is after the tail of the list, it doesn't exist
+    if( node->next == NULL ) {
+      puts("Cannot remove a node that is after the tail of the list.");
+      return -1;
+    }
+
+    printf("Deletion of arbitrary node in progress... ");
+    *data = node->next->data;
+    deleted_node = node->next;
+
+    node->next = node->next->next;
+    if( node->next == NULL ) {
+      list->tail = node;
+    }
+  }
+
+  free(deleted_node);
+  puts("Node deleted.");
+
+  list->size--;
+
+return 0;
+}
+
